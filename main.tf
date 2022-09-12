3c96c089596ea2e878dc6a5dcd8eec3cc111dbff
divyasn21 <103387570+divyasn21@users.noreply.github.com>
2022-09-12 23:24:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform{
+ module "s3_bucket" {
+   source = "terraform-aws-modules/s3-bucket/aws"
+ 
+   bucket = "my-s3-bucket"
+   acl    = "private"
+ 
+   versioning = {
+     enabled = true
+   }
+
+ }
+}
